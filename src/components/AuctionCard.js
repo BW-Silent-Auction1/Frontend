97342ab2cccf045d9ad462496a81ed4b64eca560
Eric Wood <epwood1@gmail.com>
2020-03-06 15:16:57 -0800

    finished bidding

--- src/components/AuctionCard.js
+++ src/components/AuctionCard.js
@@ -2,12 +2,23 @@ import React, {useState, useContext, useEffect} from 'react';
 import Timer from './Timer';
 import { useParams } from 'react-router-dom';
 import AuctionsContext from '../contexts/AuctionsContext';
+import UserContext from '../contexts/UserContext';
+import BidForm from './BidForm';
 
 
 const AuctionCard = props => {
     const {auctions, updateAuctions} = useContext(AuctionsContext);
+    const {user, updateUser} = useContext(UserContext);
     const {id} = useParams();
     const [auction, setAuction] = useState();
+
+    useEffect(() => {
+        if(!auctions)
+            updateAuctions();
+        
+        if(!user)
+            updateUser();
+    }, []);
 
     useEffect(() => {
         console.log(id);
@@ -35,7 +46,8 @@ const AuctionCard = props => {
             <p>{auction.endtime}</p>
             <h2>Item Description</h2>
             <p>{auction.auction_description}</p>
-            <Timer endtime={auction.endtime}/>      
+            <Timer endtime={auction.endtime}/>
+            <BidForm auction={auction} user_id={user.id} />      
         </div>
     )
 }
--- src/components/BidForm.js
+++ src/components/BidForm.js
@@ -1,32 +1,44 @@
-import React, {useState} from '.react'
+import React, {useState} from 'react';
+import axiosWithAuth from "../utils/axiosWithAuth";
+import AuctionsContext from '../contexts/AuctionsContext';
 
-const BidForm = ({auction}) => {
-    cosnt [bid, setBid] = useState(0);
+const BidForm = ({auction, user_id}) => {
+    const [bid, setBid] = useState({
+        "user_id" : user_id,
+        "auctions_id" : auction.id,
+        "amount" : auction.currentprice
+    });
+
+    const [success, setSuccess] = useState(false);
 
     const handleBid = e => {
-
+        e.preventDefault();
+        axiosWithAuth()
+            .post("/bids", bid)
+            .then( () => setSuccess(true))
+            .catch(err => console.log(`Error in handleBid: ${err}`));
     };
 
     const handleChange = e => {
         if(e.target.value >= 0)
-            setBid(e.target.value);
+            setBid({...bid, "amount": e.target.value});
     };
+
+    if(success)
+        return <h2>Your bid as been submitted</h2>
 
     return (
         <div className="bid">
-          <h1>Log in to your account</h1>
           <form onSubmit={handleBid}>
               <label>ENTER YOUR BID</label>
               <input
                 type="text"
                 name="thebid"
-                value={credentials.bid}
+                value={bid.amount}
                 onChange={handleChange}
               />
               <button>Submit Bid</button>
           </form>
-          <p>Don't have an account?</p>
-          <Link to="/register">Create a new account</Link>
         </div>
       );
 };
